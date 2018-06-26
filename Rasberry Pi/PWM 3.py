1c0bacd5611a94f25030d064d40161543e152bf8
pierre-ec <33463048+pierre-ec@users.noreply.github.com>
2018-06-26 22:32:06 +0200

    Update PWM 3.py
    Ajout de la gestion des 4 groupes de LED (une centrale, un groupe interne, un groupe intermédiaire, et un externe)

--- Rasberry Pi/PWM 3.py
+++ Rasberry Pi/PWM 3.py
@@ -3,7 +3,10 @@ import time
 
 # DECLARATIONS DES BROCHES #
 
-groupe_led1=40
+groupe_led1=8        # LED CENTRALE 
+groupe_led2=40       # GROUPE INTERIEUR
+groupe_led3=38       # GROUPE INTERMEDIAIRE
+groupe_led4=37       # GROUPE EXTERIEUR 
 
 # DECLARATIONS DES COMPTEURS #
 
@@ -17,33 +20,81 @@ compteur_tableau_frequence=0
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
-## TABLEAUX POUR LE PREMIER GROUPE DE LED ##
+## TABLEAUX POUR LA LED CENTRALE ##
 
 ### FREQUENCE EN HERTZ
 
-tableau_frequence_start1=[15, 1, 8,0]
-tableau_frequence_end1=[1, 15, 8,0]
+tableau_frequence_start1=[1, 2, 1,1,0]
+tableau_frequence_end1=[1, 20, 20,1,0]
 
 ### RAPPORT DE CYCLE (DUTY CYCLE) NOMBRE REEL > 0 & < 100
 
-tableau_dc_start1=[80.0, 10.0, 10.0,0.0]
-tableau_dc_end1=[10.0, 80.0, 90.0,0.0]
+tableau_dc_start1=[50.0, 50.0, 50.0,20.0,0.0]
+tableau_dc_end1=[40.0, 50.0, 40.0,40.0,0.0]
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+## TABLEAUX POUR LE GROUPE DE LED INTERIEUR ##
+
+### FREQUENCE EN HERTZ
+
+tableau_frequence_start2=[2, 2, 1,9,0]
+tableau_frequence_end2=[4, 20, 20,9,0]
+
+### RAPPORT DE CYCLE (DUTY CYCLE) NOMBRE REEL > 0 & < 100
+
+tableau_dc_start2=[50.0, 50.0, 50.0,50.0,0.0]
+tableau_dc_end2=[50.0, 10.0, 40.0,50.0,0.0]
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+## TABLEAUX POUR LE GROUPE DE LED INTERMEDIAIRE ##
+
+### FREQUENCE EN HERTZ
+
+tableau_frequence_start3=[2, 2, 8,9,0]
+tableau_frequence_end3=[3, 2.5, 8,9.5,0]
+
+### RAPPORT DE CYCLE (DUTY CYCLE) NOMBRE REEL > 0 & < 100
+
+tableau_dc_start3=[50.0, 50.0, 50.0,50.0,0.0]
+tableau_dc_end3=[50.0, 50.0, 40.0,50.0,0.0]
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+## TABLEAUX POUR LE GROUPE DE LED EXTERIEUR ##
+
+### FREQUENCE EN HERTZ
+
+tableau_frequence_start4=[2, 8, 1,9,0]
+tableau_frequence_end4=[2, 8.5, 20,9,0]
+
+### RAPPORT DE CYCLE (DUTY CYCLE) NOMBRE REEL > 0 & < 100
+
+tableau_dc_start4=[50.0, 50.0, 50.0,50.0,0.0]
+tableau_dc_end4=[50.0, 50.0, 35.0,50.0,0.0]
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 ## TABLEAUX DES DUREES ##
 
-tableau_duree_sequence=[0,8,12,15,0]
-duree_totale=15
+tableau_duree_sequence=[0,10,20,30,40,0]
+duree_totale=40
 
 # DECLARATIONS DES FONCTIONS D'ALLUMAGE / EXTINCTION #
 
 def setup():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(groupe_led1, GPIO.OUT)
+    GPIO.setup(groupe_led2, GPIO.OUT)
+    GPIO.setup(groupe_led3, GPIO.OUT)
+    GPIO.setup(groupe_led4, GPIO.OUT)
 
 def end():
     GPIO.output(groupe_led1, GPIO.LOW)
+    GPIO.output(groupe_led2, GPIO.LOW)
+    GPIO.output(groupe_led3, GPIO.LOW)
+    GPIO.output(groupe_led4, GPIO.LOW)
     GPIO.cleanup()
 
 # DEBUT DU PROGRAMME #
@@ -53,10 +104,28 @@ setup()
 p1= GPIO.PWM(groupe_led1, tableau_frequence_start1[compteur_tableau_frequence])
 p1.start(tableau_dc_start1[compteur_tableau_dc])
 
+p2= GPIO.PWM(groupe_led2, tableau_frequence_start2[compteur_tableau_frequence])
+p2.start(tableau_dc_start2[compteur_tableau_dc])
+
+p3= GPIO.PWM(groupe_led3, tableau_frequence_start3[compteur_tableau_frequence])
+p3.start(tableau_dc_start3[compteur_tableau_dc])
+
+p4= GPIO.PWM(groupe_led4, tableau_frequence_start4[compteur_tableau_frequence])
+p4.start(tableau_dc_start4[compteur_tableau_dc])
+
 ## CALCUL DES COMPENSATIONS ##
 
 compensation_frequence=(tableau_frequence_end1[compteur_tableau_frequence]-tableau_frequence_start1[compteur_tableau_frequence])/(tableau_duree_sequence[compteur_tableau_duree+1]-tableau_duree_sequence[compteur_tableau_duree])/1000
 compensation_dc=(tableau_dc_end1[compteur_tableau_dc]-tableau_dc_start1[compteur_tableau_dc])/(tableau_duree_sequence[compteur_tableau_duree+1]-tableau_duree_sequence[compteur_tableau_duree])/1000
+
+compensation_frequence2=(tableau_frequence_end2[compteur_tableau_frequence]-tableau_frequence_start2[compteur_tableau_frequence])/(tableau_duree_sequence[compteur_tableau_duree+1]-tableau_duree_sequence[compteur_tableau_duree])/1000
+compensation_dc2=(tableau_dc_end2[compteur_tableau_dc]-tableau_dc_start2[compteur_tableau_dc])/(tableau_duree_sequence[compteur_tableau_duree+1]-tableau_duree_sequence[compteur_tableau_duree])/1000
+
+compensation_frequence3=(tableau_frequence_end3[compteur_tableau_frequence]-tableau_frequence_start3[compteur_tableau_frequence])/(tableau_duree_sequence[compteur_tableau_duree+1]-tableau_duree_sequence[compteur_tableau_duree])/1000
+compensation_dc3=(tableau_dc_end3[compteur_tableau_dc]-tableau_dc_start3[compteur_tableau_dc])/(tableau_duree_sequence[compteur_tableau_duree+1]-tableau_duree_sequence[compteur_tableau_duree])/1000
+
+compensation_frequence4=(tableau_frequence_end4[compteur_tableau_frequence]-tableau_frequence_start4[compteur_tableau_frequence])/(tableau_duree_sequence[compteur_tableau_duree+1]-tableau_duree_sequence[compteur_tableau_duree])/1000
+compensation_dc4=(tableau_dc_end4[compteur_tableau_dc]-tableau_dc_start4[compteur_tableau_dc])/(tableau_duree_sequence[compteur_tableau_duree+1]-tableau_duree_sequence[compteur_tableau_duree])/1000
 
 ## BOUCLE PRINCIPALE 
 
@@ -66,9 +135,27 @@ while i<duree_totale:
 
         tableau_frequence_start1[compteur_tableau_frequence]=tableau_frequence_start1[compteur_tableau_frequence]+compensation_frequence
         tableau_dc_start1[compteur_tableau_dc]=tableau_dc_start1[compteur_tableau_dc]+compensation_dc
+        
+        tableau_frequence_start2[compteur_tableau_frequence]=tableau_frequence_start2[compteur_tableau_frequence]+compensation_frequence2
+        tableau_dc_start2[compteur_tableau_dc]=tableau_dc_start2[compteur_tableau_dc]+compensation_dc2
+
+        tableau_frequence_start3[compteur_tableau_frequence]=tableau_frequence_start3[compteur_tableau_frequence]+compensation_frequence3
+        tableau_dc_start3[compteur_tableau_dc]=tableau_dc_start3[compteur_tableau_dc]+compensation_dc3
+
+        tableau_frequence_start4[compteur_tableau_frequence]=tableau_frequence_start4[compteur_tableau_frequence]+compensation_frequence4
+        tableau_dc_start4[compteur_tableau_dc]=tableau_dc_start4[compteur_tableau_dc]+compensation_dc4                
    
-        p1.ChangeFrequency(tableau_frequence_start1[compteur_tableau_frequence])
-        p1.ChangeDutyCycle(tableau_dc_start1[compteur_tableau_dc])
+        p1.ChangeFrequency(tableau_frequence_start2[compteur_tableau_frequence])
+        p1.ChangeDutyCycle(tableau_dc_start2[compteur_tableau_dc])
+        
+        p2.ChangeFrequency(tableau_frequence_start2[compteur_tableau_frequence])
+        p2.ChangeDutyCycle(tableau_dc_start2[compteur_tableau_dc])
+
+        p3.ChangeFrequency(tableau_frequence_start3[compteur_tableau_frequence])
+        p3.ChangeDutyCycle(tableau_dc_start3[compteur_tableau_dc])
+
+        p4.ChangeFrequency(tableau_frequence_start4[compteur_tableau_frequence])
+        p4.ChangeDutyCycle(tableau_dc_start4[compteur_tableau_dc])
 
         time.sleep(0.001)
 
@@ -80,6 +167,15 @@ while i<duree_totale:
 
     compensation_frequence=(tableau_frequence_end1[compteur_tableau_frequence]-tableau_frequence_start1[compteur_tableau_frequence])/(tableau_duree_sequence[compteur_tableau_duree+1]-tableau_duree_sequence[compteur_tableau_duree])/1000
     compensation_dc=(tableau_dc_end1[compteur_tableau_dc]-tableau_dc_start1[compteur_tableau_dc])/(tableau_duree_sequence[compteur_tableau_duree+1]-tableau_duree_sequence[compteur_tableau_duree])/1000
+
+    compensation_frequence2=(tableau_frequence_end2[compteur_tableau_frequence]-tableau_frequence_start2[compteur_tableau_frequence])/(tableau_duree_sequence[compteur_tableau_duree+1]-tableau_duree_sequence[compteur_tableau_duree])/1000
+    compensation_dc2=(tableau_dc_end2[compteur_tableau_dc]-tableau_dc_start2[compteur_tableau_dc])/(tableau_duree_sequence[compteur_tableau_duree+1]-tableau_duree_sequence[compteur_tableau_duree])/1000
+
+    compensation_frequence3=(tableau_frequence_end3[compteur_tableau_frequence]-tableau_frequence_start3[compteur_tableau_frequence])/(tableau_duree_sequence[compteur_tableau_duree+1]-tableau_duree_sequence[compteur_tableau_duree])/1000
+    compensation_dc3=(tableau_dc_end3[compteur_tableau_dc]-tableau_dc_start3[compteur_tableau_dc])/(tableau_duree_sequence[compteur_tableau_duree+1]-tableau_duree_sequence[compteur_tableau_duree])/1000
+
+    compensation_frequence4=(tableau_frequence_end4[compteur_tableau_frequence]-tableau_frequence_start4[compteur_tableau_frequence])/(tableau_duree_sequence[compteur_tableau_duree+1]-tableau_duree_sequence[compteur_tableau_duree])/1000
+    compensation_dc4=(tableau_dc_end4[compteur_tableau_dc]-tableau_dc_start4[compteur_tableau_dc])/(tableau_duree_sequence[compteur_tableau_duree+1]-tableau_duree_sequence[compteur_tableau_duree])/1000
     
 end()
 
